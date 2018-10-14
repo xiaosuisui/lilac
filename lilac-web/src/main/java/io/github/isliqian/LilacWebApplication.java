07c46ffb3a44af8e9ba040c9ea1c322cf5509210
isliqan <51103942@qq.com>
2018-10-14 17:41:50 +0800

    修改配置

--- lilac-web/src/main/java/io/github/isliqian/LilacWebApplication.java
+++ lilac-web/src/main/java/io/github/isliqian/LilacWebApplication.java
@@ -3,7 +3,12 @@ package io.github.isliqian;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
 
 
 @SpringBootApplication(scanBasePackages = {"io.github.isliqian.*", "io.github.isliqian.log.*", "io.github.isliqian.shiro.*","io.github.isliqian.sys.*"})
@@ -13,5 +18,7 @@ public class LilacWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(LilacWebApplication.class, args);
     }
+
+
 
 }
