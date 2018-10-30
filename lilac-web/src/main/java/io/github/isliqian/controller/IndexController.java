37803451d5dfc969c2f9a270fce7a638ac8e8000
isliqan <51103942@qq.com>
2018-10-30 22:44:05 +0800

    修改界面

--- lilac-web/src/main/java/io/github/isliqian/controller/IndexController.java
+++ lilac-web/src/main/java/io/github/isliqian/controller/IndexController.java
@@ -8,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.*;
+
 @Controller
 @Api(value = "路由控制管理")
 public class IndexController {
@@ -15,10 +17,9 @@ public class IndexController {
 
     @GetMapping("/")
     @ApiOperation(value="首页")
-    public ModelAndView index(){
-      ModelAndView mav = new ModelAndView();
-      mav.setViewName("index.html");
-      return mav;
+    public String index(){
+
+      return "index.html";
     }
     @GetMapping("/signin")
     @ApiOperation(value="登陆")
@@ -35,10 +36,13 @@ public class IndexController {
     }
     @GetMapping("/home/console.html")
     @ApiOperation(value="控制面板")
-    public ModelAndView home(){
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("/home/console.html");
-        return mav;
+    public String home(Model model){
+        // 获取访问量信息
+        String basePath = System.getProperty("user.dir");
+        String txtFilePath = basePath+"\\count.txt";
+        Long count = Get_Visit_Count(txtFilePath);
+        model.addAttribute("count", count); // 后台参数传递给前端
+        return "/home/console.html";
     }
     @GetMapping("/set/system/website")
     @ApiOperation(value="网站设置")
@@ -62,5 +66,46 @@ public class IndexController {
         ModelAndView mav = new ModelAndView();
         mav.setViewName("/plug/generator.html");
         return mav;
+    }
+
+    /*
+     * 获取txt文件中的数字，即之前的访问量
+     * 传入参数为： 字符串: txtFilePath,文件的绝对路径
+     */
+    public static Long Get_Visit_Count(String txtFilePath) {
+
+        try {
+            //读取文件(字符流)
+            BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(txtFilePath),"UTF-8"));
+            //循环读取数据
+            String str = null;
+            StringBuffer content = new StringBuffer();
+            while ((str = in.readLine()) != null) {
+                content.append(str);
+            }
+            //关闭流
+            in.close();
+
+            //System.out.println(content);
+            // 解析获取的数据
+            Long count = Long.valueOf(content.toString());
+            count ++; // 访问量加1
+            //写入相应的文件
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(txtFilePath),"UTF-8"));
+            out.write(String.valueOf(count));
+
+            //清楚缓存
+            out.flush();
+            //关闭流
+            out.close();
+
+            return count;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return 0L;
+        }
+
+
     }
 }
---
+++ lilac-web/src/main/java/io/github/isliqian/controller/WxUserController.java
@@ -0,0 +1,70 @@
+package io.github.isliqian.controller;
+
+import io.github.isliqian.log.ann.MyLog;
+import io.github.isliqian.sys.base.Page;
+import io.github.isliqian.sys.bean.SysRole;
+import io.github.isliqian.sys.bean.SysUser;
+import io.github.isliqian.sys.service.SysRoleService;
+import io.github.isliqian.utils.StringUtils;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+
+/**
+ * @author wxt.liqian
+ * @version 2018/10/26
+ * @desc 微信用户
+ */
+@Controller
+@RequestMapping("/wx/user")
+public class WxUserController {
+
+    @Resource
+    private SysRoleService sysRoleService;
+
+    @GetMapping("/")
+    @ApiOperation(value="网站用户管理")
+    public ModelAndView list( HttpServletRequest request, HttpServletResponse response, ModelAndView mav ){
+
+        mav.setViewName("/wx/user.html");
+        return mav;
+    }
+
+//    @MyLog("根据id查询角色详情")
+//    @GetMapping("/info")
+//    @ApiOperation(value="根据id查询角色详情")
+//    public ModelAndView form(SysRole sysRole, ModelAndView modelAndView){
+//        if (StringUtils.isNotBlank(sysRole.getId())) {
+//            modelAndView.addObject("role",sysRoleService.get(sysRole.getId()));
+//        }else {
+//            modelAndView.addObject("role",new SysRole());
+//
+//        }
+//        modelAndView.setViewName("/sys/roleform.html");
+//        return modelAndView;
+//    }
+//
+//
+//
+//
+//    @MyLog("根据id删除角色")
+//    @GetMapping("/del")
+//    @ApiOperation(value="根据id删除角色")
+//    public ModelAndView delete(SysRole sysRole,ModelAndView modelAndView){
+//        if (StringUtils.isNotBlank(sysRole.getId())) {
+//            sysRole.setDelFlag("1");
+//            sysRole.setUpdateDate(new Date());
+//            sysRoleService.delete(sysRole);
+//            //TODO 还需删除角色关联的菜单
+//
+//        } modelAndView.setViewName("redirect:/sys/role/?repage");
+//        return modelAndView;
+//    }
+}
